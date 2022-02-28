b4633036b8ca8b05597b368cc41c423dbdeeb2f0
Miguel Ángel Durán <miduga@gmail.com>
2022-02-28 12:39:12 +0100

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import { useState } from 'react'
-
-function App() {
+function App () {
   return (
-    <div className="container">
-      
+    <div className='container'>
+
+      <div className='box'>
+        <div className='box-body'>
+          <div className='box-avatar'>
+            <img src='https://unavatar.io/twitter/midudev' alt='midudev' />
+          </div>
+          <div className='box-compose'>
+            <form>
+              <textarea
+                placeholder='¿Qué está pasando?'
+                className='box-textbox'
+                onKeyUp={() => {}}
+                onClick={() => {}}
+              />
+            </form>
+
+          </div>
+        </div>
+        <div className='box-footer'>
+          <button type='submit' className='tweet-button'>
+            Twittear
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,6 @@ import './index.css'
 import App from './App'
 
 ReactDOM.render(
-    <App />,
+  <App />,
   document.getElementById('root')
 )
